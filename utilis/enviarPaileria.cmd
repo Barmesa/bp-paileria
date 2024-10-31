db3e098e28c51b5868edbacd96647b05bebfe3ec
Gustavo de León <gustavo.deleon@barmesapumps.com>
2024-10-30 19:45:48 -0600

    Update enviarPaileria.cmd
    Ahora no es necesario buscar y arrastrar el archivo de OP (aunque sigue siendo posible).
    
    Si no se arrastra el archivo sobre el script se usa la ubicación por defecto correspondiente de ese archivo autogenerado por TOTVS

--- utilis/enviarPaileria.cmd
+++ utilis/enviarPaileria.cmd
@@ -1,11 +1,20 @@
 @echo off
 setlocal
 
-:: Si no se recibe un archivo, mostrar mensaje de ayuda
+:: Ruta predefinida del archivo
+set "archivoPorDefecto=C:\spool\odpcp.##r"
+
+:: Revisa si un archivo fue arrastrado, si no, usa el archivo por defecto
 if "%~1"=="" (
-    echo Por favor arrastra y suelta un archivo en este script para que funcione correctamente.
-    pause
-    exit /b
+    if exist "%archivoPorDefecto%" (
+        set "rutaArchivo=%archivoPorDefecto%"
+    ) else (
+        echo No se arrastro ningun archivo, y el archivo por defecto "%archivoPorDefecto" no existe.
+        pause
+        exit /b
+    )
+) else (
+    set "rutaArchivo=%~1"
 )
 
 :: URL del Worker de Cloudflare
@@ -13,9 +22,10 @@ set "url=https://bp-paileria.barmesa.workers.dev"
 
 :: Hacemos el request POST con curl y enviamos el archivo
 :: pon -v al final para ver el proceso
-curl -X POST -H "Content-Type: application/octet-stream" --data-binary "@%~1" "%url%"
+curl -X POST -H "Content-Type: application/octet-stream" --data-binary "@%rutaArchivo%" "%url%"
 
 :: Mantenemos la ventana abierta para ver la respuesta
+echo.
 echo.
 echo Archivo enviado correctamente,
 echo los codigos superiores deben de ser los correctos para agregar al archivo de paileria.
